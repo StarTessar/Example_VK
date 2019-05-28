3629fabb01993cec1e5390fc8a908c56c2f99e2b
Leontii <43552907+StarTessar@users.noreply.github.com>
2019-05-28 03:13:25 +0300

    Доработано

--- word2vec.py
+++ word2vec.py
@@ -13,42 +13,34 @@ class LoadData:
 
     @staticmethod
     def prepare_corpus(loaded_csv: pnd.DataFrame):
-        def correct_text(get_text: str):
-            result_text = get_text.replace('?', ' vopros ')
-            result_text = result_text.replace(',', ' zapat ')
-            result_text = result_text.replace('.', ' toch ')
-            result_text = result_text.replace(':', ' dwoet ')
-            result_text = result_text.replace('(', ' lev_skob ')
-            result_text = result_text.replace(')', ' pra_skob ')
-            result_text = result_text.replace('  ', ' ')
-            result_text = result_text.lower()
-            return result_text
+        """Подготовка корпуса для обучения"""
 
-        loaded_csv = loaded_csv.fillna('empty')
-
-        first_row = list(loaded_csv['question1'].dropna().values)
-        second_row = list(loaded_csv['question2'].dropna().values)
-
-        first_row = list(map(correct_text, first_row))
-        second_row = list(map(correct_text, second_row))
+        # Данные уже очищены, поэтому простое извлечение и конкатенация списка вопросов
+        first_row = list(loaded_csv['question1'].values)
+        second_row = list(loaded_csv['question2'].values)
 
         result_list = first_row + second_row
         return result_list
 
     @staticmethod
     def tokenize_corpus(corpus):
+        """Токенизация. Каждый вопрос в списке организуется в список составляющих слов"""
         tokens_list = [x.split() for x in corpus]
         return tokens_list
 
     @staticmethod
     def create_dict(corpus):
+        """Создание словаря"""
+
+        # Конкатенация в единую строку и развиение на отдельные слова. Затем отбрасывание повторений
         vocabulary = ' '.join(corpus).split(' ')
         vocabulary = set(vocabulary)
-        vocabulary.remove('')
 
+        # Построение прямого и обратного словарей
         word2idx = {w: idx for (idx, w) in enumerate(vocabulary)}
         idx2word = {idx: w for (idx, w) in enumerate(vocabulary)}
 
+        # Подсчёт слов в словаре, для контроля
         vocabulary_size = len(vocabulary)
         print('Размер словаря: {}'.format(vocabulary_size))
         del vocabulary
@@ -56,65 +48,37 @@ class LoadData:
         return word2idx, idx2word
 
     @staticmethod
-    def create_dict_old(tokenized_corpus):
-        vocabulary = []
-        for sentence in tokenized_corpus:
-            for token in sentence:
-                if token not in vocabulary:
-                    vocabulary.append(token)
-
-        word2idx = {w: idx for (idx, w) in enumerate(vocabulary)}
-        idx2word = {idx: w for (idx, w) in enumerate(vocabulary)}
-
-        vocabulary_size = len(vocabulary)
-        print('Размер словаря: {}'.format(vocabulary_size))
-
-        return word2idx, idx2word
-
-    @staticmethod
     def create_dataset(tokenized_corpus, word2idx):
-        window_size = 2
-        idx_pairs = []
+        """Создание датасета из корпуса"""
+        window_size = 2     # Ширина окна для сопоставления контекста
+        idx_pairs = []      # Инициализация списка для примеров
 
+        # Для каждого вопроса в списке
         for sentence in tokenized_corpus:
+            # Приведение предложения к численному виду в соответствии со словарём
             indices = [word2idx[word] for word in sentence]
 
+            # Для каждого слова в вопросе
             for center_word_pos in range(len(indices)):
+                # Внутри окна
                 for w in range(-window_size, window_size + 1):
+                    # Индекс контекстного слова
                     context_word_pos = center_word_pos + w
+                    # Ограничение на краевые эффекты и самоповтор
                     if context_word_pos < 0 or context_word_pos >= len(indices) or center_word_pos == context_word_pos:
                         continue
+                    # Выбор контекстного слова и сохранение в паре с текущим
                     context_word_idx = indices[context_word_pos]
                     idx_pairs.append((indices[center_word_pos], context_word_idx))
 
         idx_pairs = nmp.array(idx_pairs)
-
-        return idx_pairs
-
-    @staticmethod
-    def create_dataset_old(tokenized_corpus, word2idx):
-        window_size = 2
-        idx_pairs = []
-
-        for sentence in tokenized_corpus:
-            indices = [word2idx[word] for word in sentence]
-
-            for center_word_pos in range(len(indices)):
-                for w in range(-window_size, window_size + 1):
-                    context_word_pos = center_word_pos + w
-                    if context_word_pos < 0 or context_word_pos >= len(indices) or center_word_pos == context_word_pos:
-                        continue
-                    context_word_idx = indices[context_word_pos]
-                    idx_pairs.append((indices[center_word_pos], context_word_idx))
-
-        idx_pairs = nmp.array(idx_pairs)
-
         return idx_pairs
 
     @staticmethod
     def prepare_dataset():
+        """Метод для предварительной подготовки обучающего датасета"""
         print('Загрузка CSV')
-        loaded_data = LoadData.load_csv('train.csv')
+        loaded_data = LoadData.load_csv('quora_not_net/clean_train.csv')
         print('Подготовка корпуса')
         ready_corpus = LoadData.prepare_corpus(loaded_data)
 
@@ -128,17 +92,21 @@ class LoadData:
         print('Сохранение датасета')
         dataset = nmp.unique(dataset, axis=0)
         nmp.save('Ready_dataset.npy', dataset)
-        
+
         print('Сохранение idx2w словаря')
         nmp.save('idx2w.npy', list(word2idx_dict))
 
     @staticmethod
     def load_prepared_datas():
+        """Загрузка предварительно подготовленного датасета"""
         print('Загрузка файлов CSV')
         raw_dict = nmp.load('idx2w.npy')
         raw_dataset = nmp.load('Ready_dataset.npy')
+
+        # Перемешать не помешает
         nmp.random.shuffle(raw_dataset)
 
+        # Восстановление словаря
         word2idx = {w: idx for (idx, w) in enumerate(raw_dict)}
         del raw_dict
 
@@ -146,64 +114,100 @@ class LoadData:
 
 
 class T2Wnet:
+    """Нейронная сеть для эмбеддинга"""
     def __init__(self, vocab_size, embedding_len, learn_rate, batch_size):
+        """
+        Инициализация сети
+        :param vocab_size: Размер словаря
+        :param embedding_len: Длина эмбеддинга
+        :param learn_rate: Параметр начальной скорости обучения
+        :param batch_size: Размер блока данных для итерации
+        """
+
+        # Плейсхолдеры для датасета
         self.input_ph = tf.placeholder(dtype=tf.int32, shape=None)
         self.output_ph = tf.placeholder(dtype=tf.int32, shape=[None, 1])
 
+        # Организация датасета и итератора из входных векторов
         dataset = tf.data.Dataset.from_tensor_slices((self.input_ph, self.output_ph)).cache()
         dataset = dataset.repeat().batch(batch_size)
         self.iterate = dataset.make_initializable_iterator()
         input_batch, output_batch = self.iterate.get_next()
 
+        # Создание слоя для эмбеддинга. В него будет смотреть сеть для воспроизведения слов в векторной форме
         self.embeddings = tf.Variable(
             tf.random_uniform([vocab_size, embedding_len], -1.0, 1.0))
+
+        # Организация второго слоя
         softmax_weights = tf.Variable(
             tf.truncated_normal([vocab_size, embedding_len],
                                 stddev=1.0 / nmp.math.sqrt(embedding_len)))
         softmax_biases = tf.Variable(tf.zeros([vocab_size]))
 
+        # Векторизация числового представления слов
         embed = tf.nn.embedding_lookup(self.embeddings, input_batch)
+        # Расчёт функции потерь
         loss = tf.reduce_mean(
             tf.nn.sampled_softmax_loss(weights=softmax_weights, biases=softmax_biases, inputs=embed,
                                        labels=output_batch, num_sampled=64, num_classes=vocab_size))
+        tf.summary.scalar("loss", loss)
 
+        # Глобальный счётчик итераций
         with tf.variable_scope("gs"):
             self.global_step = tf.train.get_or_create_global_step()
 
-        tf.summary.scalar("xent_1", loss)
-
+        # Вычисление текущего значения скорости обучения. Для улучшения сходимости сделал её ниспадающей со временем
         self.loc_lear = learn_rate * tf.pow(tf.cast(0.1, tf.float64), self.global_step / 4000 + 1)
+        tf.summary.scalar("learning_rate", self.loc_lear)
+        # Оптимизация весов
         self.optim = tf.train.AdamOptimizer(learning_rate=self.loc_lear).minimize(loss,
                                                                                   global_step=self.global_step)
-        tf.summary.scalar("learning_rate", self.loc_lear)
 
-    def teach(self, epo, loc_dataset):
+    def teach(self, limit, loc_dataset_raw):
+        """Метод для запуска процесса обучения"""
+        # Создание новой сессии
         sv = tf.Session()
         with sv as sess:
+            # Инициализация
             summ = tf.summary.merge_all()
             sess.run(tf.global_variables_initializer())
 
-            sess.run(self.iterate.initializer, feed_dict={self.input_ph: loc_dataset[:, 0],
-                                                          self.output_ph: loc_dataset[:, 1, nmp.newaxis]})
+            # Перемешивание датасета и инициализация итератора
+            loc_dataset = loc_dataset_raw
+            nmp.random.shuffle(loc_dataset)
+            sess.run(self.iterate.initializer, feed_dict={self.input_ph: loc_dataset[:, 1],
+                                                          self.output_ph: loc_dataset[:, 0, nmp.newaxis]})
 
+            # Подготовка логирования
             writer = tf.summary.FileWriter('C:/TF/Log/' + 'Vars' + self.get_log_directory())
             writer.add_graph(sess.graph)
 
-            limit = epo
+            # Главный обучающий цикл
             gs = 0
             while gs < limit - 1:
+                # Для удобства прикрутил прогрессбар
                 for _ in tqdm(range(limit),
                               total=limit,
                               ncols=100,
                               leave=False,
                               unit='b'):
+
+                    # Выполнение итерации
                     gs_loc, _ = sess.run([self.global_step, self.optim])
 
+                    # Логирование
                     s = sess.run(summ)
                     writer.add_summary(s, gs)
 
                     gs += 1
 
+                    # Для удаления цикличности данных они перемешиваются после каждой эпохи
+                    if gs % (len(dataset_val) // batch_size_of) == 0:
+                        nmp.random.shuffle(loc_dataset)
+                        sess.run(self.iterate.initializer, feed_dict={self.input_ph: loc_dataset[:, 1],
+                                                                      self.output_ph: loc_dataset[:, 0, nmp.newaxis]})
+
+            # Последняя запись в лог и сохранение весов из скрытого слоя эмбеддинга
             s = sess.run(summ)
             writer.add_summary(s, gs)
 
@@ -224,12 +228,24 @@ class T2Wnet:
 
 
 if __name__ == '__main__':
+    # Предварительная подготовка датасета
+    LoadData.prepare_dataset()
+
+    # Загрузка соранённого датасета
     dataset_val, word2idx_dict = LoadData.load_prepared_datas()
 
+    # Установка параметров для обучения. Размер блока для итерации, число эпох и длина эмбеддинга
     voc_size = len(word2idx_dict)
-    batch_size_of = 2000
-    t2w_net = T2Wnet(voc_size, 350, 1E-2, batch_size_of)
-    wei = t2w_net.teach(len(dataset_val) // batch_size_of, dataset_val)
+    batch_size_param = 2000
+    epo_train_param = 5
+    embedding_len_param = 350
+    learn_rate_param = 1E-2
 
+    # Инициализация сети
+    t2w_net = T2Wnet(voc_size, embedding_len_param, learn_rate_param, batch_size_param)
+    # Запуск обучения
+    wei = t2w_net.teach(epo_train_param * len(dataset_val) // batch_size_param, dataset_val)
+
+    # Сохранение словаря эмбеддингов
     nmp.save('Result_embeddings', wei)
     pass

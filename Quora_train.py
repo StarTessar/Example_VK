aed63b03d02e8e5de2bd600d30c33ccf9545cbbe
Leontii <43552907+StarTessar@users.noreply.github.com>
2019-05-28 03:45:22 +0300

    Добавил пояснения, немного почистил код

--- Quora_train.py
+++ Quora_train.py
@@ -13,59 +13,61 @@ class LoadData:
 
     @staticmethod
     def prepare_corpus(loaded_csv: pnd.DataFrame):
-        def correct_text(get_text: str):
-            result_text = get_text.replace('?', ' vopros ')
-            result_text = result_text.replace(',', ' zapat ')
-            result_text = result_text.replace('.', ' toch ')
-            result_text = result_text.replace(':', ' dwoet ')
-            result_text = result_text.replace('(', ' lev_skob ')
-            result_text = result_text.replace(')', ' pra_skob ')
-            result_text = result_text.replace('  ', ' ')
-            result_text = result_text.lower()
-            return result_text
+        """Подготовка корпуса для обучения. Методы для эмбеддинга и для пар вопросов разные"""
 
-        loaded_csv = loaded_csv.fillna('empty')
-
+        # Данные уже очищены, поэтому простое извлечение и конкатенация списка вопросов
         first_row = list(loaded_csv['question1'].dropna().values)
         second_row = list(loaded_csv['question2'].dropna().values)
 
-        first_row = list(map(correct_text, first_row))
-        second_row = list(map(correct_text, second_row))
-
+        # Возвращает два списка вопросов
         return first_row, second_row
 
     @staticmethod
     def tokenize_corpus(corpus):
+        """Токенизация. Каждый вопрос в списке организуется в список составляющих слов"""
         tokens_list = [x.split() for x in corpus]
         return tokens_list
 
     @staticmethod
     def vector_corpus(tokenized_corpus, vocab):
+        """Представление слов в виде индексов словаря"""
         new_corpus = []
 
+        # Для каждого вопроса
         for sentence in tokenized_corpus:
+            # Контроль исключений
             while 'nan' in sentence:
                 sentence.remove('nan')
             while 'null' in sentence:
                 sentence.remove('null')
             if len(sentence) == 0:
                 sentence.append('empty')
+                # Сохранение в виде списка индексов
             new_corpus.append([vocab[word] for word in sentence if word is not 'nan'])
 
         return new_corpus
 
     @staticmethod
     def load_dense_vocab():
+        """Загрузка словаря эмбеддингов"""
         raw_weis = nmp.load('Result_embeddings.npy')
+        # Контроль ориентации измерений
         if raw_weis.shape[1] > 1000:
             raw_weis = nmp.rot90(raw_weis)
         return raw_weis
 
     @staticmethod
     def vector_sent(arr_of_wordvect: list, dense_vocab: dict):
+        """Векторизация вопроса в соответствии с эмбеддингами"""
         new_array = []
+
+        # Для каждого вопроса
         for line in arr_of_wordvect:
+            # Список слов в виде сжатых векторов
             word_vectors = [dense_vocab[word] for word in line]
+            # Суммирование векторных представлений слов в вопросе.
+            #   В моём понимании это должно давать сумму их семантических значений, что является смыслом предложения
+            #   Этот подход не учитывает последовательность слов, которая бывает критична, но зато прост в применении
             line_vector = sum(word_vectors)
             new_array.append(line_vector)
 
@@ -73,18 +75,20 @@ class LoadData:
 
     @staticmethod
     def get_train_data():
+        """Метод для предварительной подготовки обучающего датасета"""
         print('Загрузка CSV')
-        loaded_data = LoadData.load_csv('train.csv')
+        loaded_data = LoadData.load_csv('quora_not_net/clean_train.csv')
         print('Подготовка корпуса')
         first_q, second_q = LoadData.prepare_corpus(loaded_data)
         print('Токенизация')
         first_q_token = LoadData.tokenize_corpus(first_q)
         second_q_token = LoadData.tokenize_corpus(second_q)
 
+        # Восстановление словаря
         raw_dict = nmp.load('idx2w.npy')
         word2idx = {w: idx for (idx, w) in enumerate(raw_dict)}
-
         del raw_dict
+
         print('Векторизация')
         first_q_vect = LoadData.vector_corpus(first_q_token, word2idx)
         second_q_vect = LoadData.vector_corpus(second_q_token, word2idx)
@@ -101,20 +105,21 @@ class LoadData:
 
     @staticmethod
     def load_ready_train_data():
+        """Загрузка предварительно подготовленного датасета"""
         first_q_res = nmp.load('quora_train_data/q1.npy')
         second_q_res = nmp.load('quora_train_data/q2.npy')
         dub_arr = nmp.load('quora_train_data/ans.npy')
 
+        # Нормализация данных по каждому измерению, чтобы сети было проще работать
         norm_param_mean = nmp.vstack([first_q_res, second_q_res]).mean(axis=0)
-
         first_q_res = (first_q_res - norm_param_mean)
         second_q_res = (second_q_res - norm_param_mean)
 
         norm_param_std = nmp.vstack([first_q_res, second_q_res]).std(axis=0)
-
         first_q_res = (first_q_res / norm_param_std)
         second_q_res = (second_q_res / norm_param_std)
 
+        # Перемешивание датасета
         rnd_state = nmp.random.get_state()
         nmp.random.shuffle(first_q_res)
         nmp.random.set_state(rnd_state)
@@ -126,119 +131,147 @@ class LoadData:
 
 
 class AnsPredict:
+    """Нейронная сеть для сравнения вопросов"""
     def __init__(self, embedding_len, learn_rate, batch_size):
+        """
+        Инициализация сети
+        :param embedding_len: Длина эмбеддинга
+        :param learn_rate: Параметр начальной скорости обучения
+        :param batch_size: Размер блока данных для итерации
+        """
+
+        # Плейсхолдеры для датасета
         self.input_ph_q1 = tf.placeholder(dtype=tf.float32, shape=[None, embedding_len])
         self.input_ph_q2 = tf.placeholder(dtype=tf.float32, shape=[None, embedding_len])
         self.output_ph = tf.placeholder(dtype=tf.int32, shape=[None])
 
+        # Организация датасета и итератора из входных векторов
         dataset = tf.data.Dataset.from_tensor_slices((self.input_ph_q1, self.input_ph_q2, self.output_ph)).cache()
         dataset = dataset.repeat().batch(batch_size)
         self.iterate = dataset.make_initializable_iterator()
         input_batch_q1, input_batch_q2, output_batch = self.iterate.get_next()
 
-        len_q1 = tf.sqrt(tf.reduce_sum(tf.square(input_batch_q1)))
-        len_q2 = tf.sqrt(tf.reduce_sum(tf.square(input_batch_q2)))
-        cos_dist = tf.reduce_sum(input_batch_q1 * input_batch_q2) / (len_q1 * len_q2)
+        # В качестве источника информации для сети я взял матрицу, которая основана на пересечении данных двух векторов.
+        #   Для человека это не будет иметь никакого смысла,
+        #   но я предположил, что сеть сможет обнаружить закономерности.
+        attention_plane = tf.matmul(input_batch_q1[:, :, tf.newaxis], input_batch_q2[:, :, tf.newaxis],
+                                    adjoint_b=True)
+        attention_plane = attention_plane[:, :, :, tf.newaxis]
 
-        dim_dist = input_batch_q1 - input_batch_q2
+        # Для извлечения карт признаков определён свёрточный слой
+        lay_1 = tf.layers.conv2d(inputs=attention_plane,
+                                 filters=5,
+                                 kernel_size=[5, 5],
+                                 strides=[5, 5],
+                                 activation=None)
 
-        lay_q1 = tf.layers.dense(inputs=input_batch_q1,
-                                 units=embedding_len,
-                                 activation=tf.nn.sigmoid)
+        # Развёртывание для полносвязного слоя
+        lay_1_flat = tf.layers.flatten(lay_1)
 
-        lay_q2 = tf.layers.dense(inputs=input_batch_q2,
-                                 units=embedding_len,
-                                 activation=tf.nn.sigmoid)
+        # Дропаут для устойчивости к шумам
+        lay_1_flat = tf.layers.dropout(lay_1_flat, 0.1)
 
-        lay_q1 = lay_q1 * input_batch_q1
-        lay_q2 = lay_q2 * input_batch_q2
-
-        lay_q = lay_q1 * lay_q2 * dim_dist
-
-        # attention_plane = tf.matmul(lay_q1[:, :, tf.newaxis], lay_q2[:, :, tf.newaxis],
-        #                             adjoint_b=True)
-        # attention_plane = attention_plane[:, :, :, tf.newaxis]
-        #
-        # lay_1 = tf.layers.conv2d(inputs=attention_plane,
-        #                          filters=5,
-        #                          kernel_size=[5, 5],
-        #                          strides=[5, 5],
-        #                          activation=None)
-
-        # lay_1 = tf.layers.conv2d(inputs=lay_1,
-        #                          filters=10,
-        #                          kernel_size=[5, 5],
-        #                          strides=[1, 1],
-        #                          activation=tf.nn.relu)
-
-        # lay_1_flat = tf.layers.flatten(lay_1)
-
-        lay_q_flat = tf.layers.dropout(lay_q, 0.1)
-
-        lay_1_flat = lay_q_flat
-
+        # Полносвязный скрытый слой
         lay_2 = tf.layers.dense(inputs=lay_1_flat,
-                                units=3000,
+                                units=900,
                                 activation=tf.nn.relu)
 
+        # Выходной слой
         lay_3 = tf.layers.dense(inputs=lay_2,
                                 units=1,
                                 activation=tf.nn.sigmoid)
 
+        # Контроль распределения ответов в логе
         lay_3 = tf.reduce_mean(lay_3, 1)
-        tf.summary.histogram("mean_ans", lay_3)
+        tf.summary.histogram("distr_ans", lay_3)
 
+        # Ввод контроля распределения ответов. Чем меньше разброс вокруг среднего, тем больше значение
         all_mean = tf.reduce_max(2 / tf.exp(lay_3 - tf.reduce_mean(lay_3)))
         tf.summary.histogram("all_simult", all_mean)
 
+        # Глобальный счётчик итераций
         with tf.variable_scope("gs"):
             self.global_step = tf.train.get_or_create_global_step()
 
-        self.xent = tf.reduce_mean(tf.losses.log_loss(labels=output_batch, predictions=lay_3)) * all_mean
-        tf.summary.scalar("xent_1", self.xent)
+        # Расчёт функции потерь. Здесь, помимо потерь самих ответов сеть наказывается и за поиск одного среднего ответа
+        #   Удовлетворяющего заданному условию
+        self.loss_func = tf.reduce_mean(tf.losses.log_loss(labels=output_batch, predictions=lay_3)) * all_mean
+        tf.summary.scalar("loss_func", self.loss_func)
 
+        # Расчёт метрики
         logloss = tf.reduce_max(tf.losses.log_loss(labels=output_batch, predictions=lay_3))
         tf.summary.scalar("logloss", logloss)
 
-        self.correct_prediction = tf.abs(lay_3 - tf.cast(output_batch, tf.float32))
-        self.accuracy = 1 - tf.reduce_mean(tf.cast(self.correct_prediction, tf.float32))
-        tf.summary.scalar("accuracy", self.accuracy)
-
+        # Вычисление текущего значения скорости обучения
         self.loc_lear = learn_rate * tf.pow(tf.cast(0.1, tf.float64), self.global_step / 500 + 1)
-        self.optim = tf.train.AdamOptimizer(learning_rate=self.loc_lear).minimize(self.xent,
-                                                                                  global_step=self.global_step)
         tf.summary.scalar("learning_rate", self.loc_lear)
+        # Оптимизация весов
+        self.optim = tf.train.AdamOptimizer(learning_rate=self.loc_lear).minimize(self.loss_func,
+                                                                                  global_step=self.global_step)
 
-    def teach(self, epo, loc_dataset):
+    def teach(self, limit, loc_dataset):
+        """Метод для запуска процесса обучения"""
+        # Создание новой сессии
         sv = tf.Session()
         with sv as sess:
+            # Инициализация
             summ = tf.summary.merge_all()
             sess.run(tf.global_variables_initializer())
 
-            sess.run(self.iterate.initializer, feed_dict={self.input_ph_q1: loc_dataset[0],
-                                                          self.input_ph_q2: loc_dataset[1],
-                                                          self.output_ph: loc_dataset[2]})
+            # Перемешивание датасета и инициализация итератора
+            ph_q1 = loc_dataset[0]
+            ph_q2 = loc_dataset[1]
+            ph_out = loc_dataset[2]
 
+            rnd_state = nmp.random.get_state()
+            nmp.random.shuffle(ph_q1)
+            nmp.random.set_state(rnd_state)
+            nmp.random.shuffle(ph_q2)
+            nmp.random.set_state(rnd_state)
+            nmp.random.shuffle(ph_out)
+
+            sess.run(self.iterate.initializer, feed_dict={self.input_ph_q1: ph_q1,
+                                                          self.input_ph_q2: ph_q2,
+                                                          self.output_ph: ph_out})
+
+            # Подготовка логирования и сохранения сети
             saver = tf.train.Saver()
             writer = tf.summary.FileWriter('C:/TF/Log/' + 'quora' + self.get_log_directory())
             writer.add_graph(sess.graph)
 
-            limit = epo
+            # Главный обучающий цикл
             gs = 0
             while gs < limit - 1:
+                # Прогрессбар
                 for _ in tqdm(range(limit),
                               total=limit,
                               ncols=100,
                               leave=False,
                               unit='b'):
-                    # for data, target in loc_dataset:
+
+                    # Выполнение итерации
                     gs_loc, _ = sess.run([self.global_step, self.optim])
 
+                    # Логирование
                     s = sess.run(summ)
                     writer.add_summary(s, gs)
 
                     gs += 1
 
+                    # Для удаления цикличности данных они перемешиваются после каждой эпохи
+                    if gs % (len(loc_dataset) // batch_size_param) == 0:
+                        rnd_state = nmp.random.get_state()
+                        nmp.random.shuffle(ph_q1)
+                        nmp.random.set_state(rnd_state)
+                        nmp.random.shuffle(ph_q2)
+                        nmp.random.set_state(rnd_state)
+                        nmp.random.shuffle(ph_out)
+
+                        sess.run(self.iterate.initializer, feed_dict={self.input_ph_q1: ph_q1,
+                                                                      self.input_ph_q2: ph_q2,
+                                                                      self.output_ph: ph_out})
+
+            # Последняя запись в лог и сохранение весов обученной сети
             s = sess.run(summ)
             writer.add_summary(s, gs)
             saver.save(sess, 'C:/TF/vk/save' + '/ckp')
@@ -258,11 +291,19 @@ class AnsPredict:
 
 
 if __name__ == '__main__':
+    # Предварительная подготовка датасета
     LoadData.get_train_data()
 
-    batch_size_of = 10000
+    # Загрузка соранённого датасета
     main_dataset = LoadData.load_ready_train_data()
-    netw = AnsPredict(main_dataset[0].shape[1], 1E-2, batch_size_of)
-    netw.teach(1000, main_dataset)
-    # netw.teach(len(main_dataset[0]) // batch_size_of, main_dataset)
+    
+    # Установка параметров для обучения. Размер блока для итерации и число эпох
+    batch_size_param = 100
+    epo_train_param = 5
+    learn_rate_param = 1E-2
+
+    # Инициализация сети
+    netw = AnsPredict(main_dataset[0].shape[1], learn_rate_param, batch_size_param)
+    # Запуск обучения
+    netw.teach(epo_train_param * len(main_dataset[0]) // batch_size_param, main_dataset)
     pass
